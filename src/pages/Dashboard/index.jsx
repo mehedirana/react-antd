ef011d3cf3b5c2bd2f5b23a3c3cf85f3eea924e2
mehedirana <mdmehedirana@gmail.com>
2024-04-15 17:14:06 +0600

    dashboard two card added

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Card, Flex, Space, Typography, Row, Col, Calendar, theme } from 'antd';
-import { header } from './struct';
+import { Card, Flex, Space, Typography, Row, Col, Calendar, theme, Avatar } from 'antd';
+import { header, title } from './struct';
 
 const { Text } = Typography
 
@@ -9,16 +9,16 @@ const { Text } = Typography
 const Dashboard = () => {
     const { token } = theme.useToken();
     const wrapperStyle = {
-      width: '100%',
-      border: `1px solid ${token.colorBorderSecondary}`,
-      borderRadius: token.borderRadiusLG,
+        width: '100%',
+        border: `1px solid ${token.colorBorderSecondary}`,
+        borderRadius: token.borderRadiusLG,
     };
 
     console.log('called');
 
     const onPanelChange = (value, mode) => {
         console.log(value.format('YYYY-MM-DD'), mode);
-      };
+    };
 
     return (
         <Row gutter={16}>
@@ -26,13 +26,36 @@ const Dashboard = () => {
                 <Card title="Upcoming LAB Class" bordered={false}>
                     Card content
                     <div style={wrapperStyle}>
-                        <Calendar fullscreen={false} onPanelChange={(e)=>onPanelChange(e)} />
+                        <Calendar fullscreen={false} onPanelChange={(e) => onPanelChange(e)} />
                     </div>
+                </Card>
+                <br/>
+                <Card style={{ width: '100%', backgroundColor:'#E3F5FF' }}>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                </Card>
+                <br/>
+                <Card style={{ width: '100%', backgroundColor:'#E5ECF6' }}>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
                 </Card>
             </Col>
             <Col span={12}>
                 <Card title="LAB Discussion" bordered={false}>
-                    Card content
+                    <Flex style={{ gap: 10 }}>
+                        <Avatar style={{ backgroundColor: 'red', verticalAlign: 'middle' }} size="large" gap={4}>
+                            dp
+                        </Avatar>
+                        <Flex vertical>
+                            <Text>Sara Ali</Text>
+                            <Text>Dept Head of CSE</Text>
+                        </Flex>
+                    </Flex>
+                    <br />
+                    <Text {...title}>What is an ER diagram, and what are its types/uses?</Text>
+                    <br />
                 </Card>
             </Col>
         </Row>
--- src/pages/Dashboard/struct.js
+++ src/pages/Dashboard/struct.js
@@ -1,9 +1,19 @@
 export const header = {
- style:{
-    color: "#101010",
-    fontFamily: "Inter",
-    fontSize: "26px",
-    fontStyle: "normal",
-    fontWeight: "600",
- }
+   style: {
+      color: "#101010",
+      fontFamily: "Inter",
+      fontSize: "26px",
+      fontStyle: "normal",
+      fontWeight: "600",
+   }
+}
+
+export const title = {
+   style: {
+      color: "#101010",
+      fontFamily: "Rubik",
+      fontSize: "24px",
+      fontWeight: 600
+
+   }
 }
